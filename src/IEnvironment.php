ea11876f008b26f46d641690166c3b400a3f4732
Wu Ming-Hsueh <ri3567@gmail.com>
2018-08-08 11:47:07 +0800

    Initial set of project template

---
+++ src/IEnvironment.php
@@ -0,0 +1,10 @@
+<?php
+namespace BaseTemplatePHP;
+
+interface IEnvironment
+{
+    const PROJECT_NAME = "BaseTemplePHP";
+    const ROUTER_START = "/BaseTemplePHP/index.php";
+    const NAMESPACE_ROOT = "BaseTemplatePHP";
+    // const CONNECTION_NAME = ["default", "sqlserver_dverental"];
+}
